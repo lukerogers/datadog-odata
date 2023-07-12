3a72a044dbcb7c433df96af9ab801a18f2a30b86
Luke <luke.rogers@gmail.com>
2023-07-12 16:57:35 -0600

    Removing unnecessary code

--- Program.cs
+++ Program.cs
@@ -1,29 +1,11 @@
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-builder.Services.AddHealthChecks()
-    .AddCheck("healthz", () => HealthCheckResult.Healthy())
-    .AddCheck("readyz", () => HealthCheckResult.Healthy(), new[] { "readiness" });
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHealthChecks("/healthz", new HealthCheckOptions
-{
-    Predicate = r => r.Name.Contains("healthz")
-});
-
-app.UseHealthChecks("/readyz", new HealthCheckOptions
-{
-    Predicate = r => r.Tags.Contains("readiness")
-});
-
 app.UseAuthorization();
 
 app.MapControllers();
